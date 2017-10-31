5bb6a4dbb891540aef61ba171c2a79b3d0794427
Luke Hsiao <lwhsiao@stanford.edu>
2017-10-30 22:19:23 -0700

    Add data directory and download script
    Rather than mirror the data in the repo, this commit adds a script to
    download the outside and inside sniffing tcpdump files from the 1999
    DARPA dataset directly from MIT.
    
    Note that it appears a few of the testing archives are actually corrupt,
    and we may need to either find mirrors of the valid files, or we may
    even need a new dataset. My initial searches didn't find any other
    mirrors of the 1999 DARPA dataset though.

---
+++ data/download-data.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# URL format:
+# https://www.ll.mit.edu/ideval/data/1999/training/week2/monday/outside.tcpdump.gz
+
+base_url="https://www.ll.mit.edu/ideval/data/1999/"
+
+mkdir -p training
+mkdir -p testing
+
+echo "Downloading the DARPA 1999 IDS Dataset..."
+
+for dataset in "testing"; do
+  if [ $dataset = "training" ]; then
+    for week in "week1" "week2" "week3"; do
+      for weekday in "monday" "tuesday" "wednesday" "thursday" "friday"; do
+
+        echo wget $base_url$dataset/$week/$weekday/outside.tcpdump.gz -O $dataset/$week\_$weekday\_outside.gz
+        wget $base_url$dataset/$week/$weekday/outside.tcpdump.gz -O $dataset/$week\_$weekday\_outside.gz -q
+        gunzip $dataset/$week\_$weekday\_outside.gz
+        
+        echo wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz -O $dataset/$week\_$weekday\_inside.gz
+        wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz -O training/$week\_$weekday\_inside.gz -q
+        gunzip $dataset/$week\_$weekday\_inside.gz
+      done
+    done
+  else 
+    for week in "week4" "week5"; do
+      for weekday in "monday" "tuesday" "wednesday" "thursday" "friday"; do
+
+        echo wget $base_url$dataset/$week/$weekday/outside.tcpdump.gz -O $dataset/$week\_$weekday\_outside.gz
+        wget $base_url$dataset/$week/$weekday/outside.tcpdump.gz -O $dataset/$week\_$weekday\_outside.gz -q
+        gunzip $dataset/$week\_$weekday\_outside.gz
+
+        echo wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz -O $dataset/$week\_$weekday\_inside.gz
+        wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz -O training/$week\_$weekday\_inside.gz -q
+        gunzip $dataset/$week\_$weekday\_inside.gz
+      done
+    done
+  fi
+done
+
+echo "Done!"
+
