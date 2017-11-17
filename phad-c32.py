38cdb35be43f309170fd5c014e811b9aa61e3647
Luke Hsiao <lwhsiao@stanford.edu>
2017-11-16 20:23:56 -0800

    Use the speed up clustering
    Uses the new parallelized np_parse_pcap function, and also
    serializes the results for easy loading if the process is
    run again.

--- phad-c32.py
+++ phad-c32.py
@@ -1,85 +1,73 @@
 #! /usr/bin/python
 """This code recreates the PHAD-C32 experiments in the original PHAD paper."""
+import numpy as np
 from pprint import pprint
-from utils.parser import np_parse_pcap
 from utils import Clusterer
+from utils.parser import np_parse_pcap
 
 
 def _clusterAndScore(days):
-    NUM_FIELDS = 33
-    (
-        etherSize,
-        etherDestHi,
-        etherDestLo,
-        etherSrcHi,
-        etherSrcLo,
-        etherProto,
-        IPHdrLen,
-        IPTOS,
-        IPLen,
-        IPFragID,
-        IPFragPtr,
-        IPTTL,
-        IPProto,
-        IPCksm,
-        IPSrc,
-        IPDest,
-        TCPSrcPort,
-        TCPDestPort,
-        TCPSeq,
-        TCPAck,
-        TCPHdrLen,
-        TCPFlags,
-        TCPWndSz,
-        TCPCksm,
-        TCPUrgPtr,
-        TCPOption,
-        UDPSrcPort,
-        UDPDestPort,
-        UDPLen,
-        UDPCksm,
-        ICMPType,
-        ICMPCode,
-        ICMPCksm
-    ) = (Clusterer() for i in range(NUM_FIELDS))
-    Clusters = (etherSize, etherDestHi, etherDestLo, etherSrcHi, etherSrcLo,
-                etherProto, IPHdrLen, IPTOS, IPLen, IPFragID, IPFragPtr, IPTTL,
-                IPProto, IPCksm, IPSrc, IPDest, TCPSrcPort, TCPDestPort,
-                TCPSeq, TCPAck, TCPHdrLen, TCPFlags, TCPWndSz, TCPCksm,
-                TCPUrgPtr, TCPOption, UDPSrcPort, UDPDestPort, UDPLen, UDPCksm,
-                ICMPType, ICMPCode, ICMPCksm)
+    print("Clustering the header fields...")
+
+    feature_keys = ['Ethernet_size', 'Ethernet_dstHi', 'Ethernet_dstLow',
+                    'Ethernet_srcHi', 'Ethernet_srcLow', 'Ethernet_type',
+                    'IPv4_ihl', 'IPv4_tos', 'IPv4_length', 'IPv4_id',
+                    'IPv4_offset', 'IPv4_ttl', 'IPv4_proto', 'IPv4_chksum',
+                    'IPv4_src', 'IPv4_dst', 'ICMP_type', 'ICMP_code',
+                    'ICMP_chksum', 'TCP_sport', 'TCP_dport', 'TCP_seqNo',
+                    'TCP_ackNo', 'TCP_dataOffset', 'TCP_flags', 'TCP_window',
+                    'TCP_chksum', 'TCP_urgPtr', 'TCP_options', 'UDP_sport',
+                    'UDP_dport', 'UDP_length', 'UDP_chksum']
+    features = {key: Clusterer() for key in feature_keys}
 
     for day in days:
         for packet_hdrs in day[0]:
-            for i in xrange(NUM_FIELDS):
+            # Iterate over feature_keys so indexes are the correct order
+            for i, feature in enumerate(feature_keys):
                 if packet_hdrs[i] != -1:
-                    Clusters[i].add(packet_hdrs[i])
+                    # NOTE(lwhsiao): Right now we're just doing this all
+                    # together. One potential way to parallelize is to instead
+                    # update each Clusterer in parallel, providing a single
+                    # column of packet headers to each, rather than processing
+                    # them all together.
+                    features[feature].add(packet_hdrs[i])
 
-    for i in xrange(NUM_FIELDS):
-        pprint(str(Clusters[i].getDistinct()) + "/" +
-               str(Clusters[i].getTotal()))
-        #  pprint(Clusters[i].getClusters())
+    model = {}
+    for feature in features:
+        model[feature] = (features[feature].getDistinct(),
+                          features[feature].getTotal())
+    pprint(model)
 
 
 def _parseTrainingData():
     """Parse the week 3 training data."""
-    # Parse the Training Data
-    w3_m = np_parse_pcap("./data/training/week3_monday_inside")
-    w3_me = np_parse_pcap("./data/training/week3_monday_extra_inside")
-    w3_t = np_parse_pcap("./data/training/week3_tuesday_inside")
-    w3_te = np_parse_pcap("./data/training/week3_tuesday_extra_inside")
-    w3_w = np_parse_pcap("./data/training/week3_wednesday_inside")
-    w3_we = np_parse_pcap("./data/training/week3_wednesday_extra_inside")
-    w3_th = np_parse_pcap("./data/training/week3_thursday_inside")
-    w3_f = np_parse_pcap("./data/training/week3_friday_inside")
+    try:
+        week3_data = np.load(open("data/week3_data.npy", "rb"))
+        print("Loaded pre-parsed training data...")
+    except IOError:
+        print("Parsing the training data...")
+        # Parse the Training Data
+        trainingFiles = [
+            "./data/training/week3_monday_inside",
+            "./data/training/week3_monday_extra_inside",
+            "./data/training/week3_tuesday_inside",
+            "./data/training/week3_tuesday_extra_inside",
+            "./data/training/week3_wednesday_inside",
+            "./data/training/week3_wednesday_extra_inside",
+            "./data/training/week3_thursday_inside",
+            "./data/training/week3_friday_inside"
+        ]
+        week3_data = np_parse_pcap(trainingFiles)
 
-    return (w3_m, w3_me, w3_t, w3_te, w3_w, w3_we, w3_th, w3_f)
+        np.save(open("data/week3_data.npy", "wb"), week3_data)
+
+    return week3_data
 
 def main():
     """Run the PHAD-C32 experiment."""
     week3_data = _parseTrainingData()
 
-    # Clustering header data
+    # Clust ering header data
     _clusterAndScore(week3_data)
 
     return
