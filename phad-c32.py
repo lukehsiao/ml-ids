e9d368e5c83a53968ccf61d5cf62555848d9c2d1
Luke Hsiao <lwhsiao@stanford.edu>
2017-12-07 22:01:05 -0800

    Just IPv4_ttl and IPv4_offset

--- phad-c32.py
+++ phad-c32.py
@@ -153,11 +153,16 @@ def _runScoring(clusters, testData):
                                             nr[feature][1])
                     lastAnomaly[feature] = timestamp
 
+        # Zero all but IPv4_ttl (idx = 11)
+        scores[:, :10] = 0
+        scores[:, 12:] = 0
+
         # Score the packet and store as last element
         scores[:, -1] = np.sum(scores[:, 0:-1], axis=1)
 
+
         # If the total score of the packet is very small, set it to one so
-        # that the resulting normalization doesn't fail.
+        # that taking the log later doesn't fail.
         scores[:, -1][scores[:, -1] < 1] = 1
 
         results = np.hstack((testData, scores))
@@ -173,13 +178,6 @@ def _outputToCSV(results, filename, threshold=0.5, feat=None):
 
     outfile = open(filename, "wb")
     writer = csv.writer(outfile)
-
-    # Zero out a feature
-    if feat:
-        scoreMat = results[:, 34:]
-        results[:, -1] -= scoreMat[:, feat]
-        scoreMat[:, feat] = 0
-        scoreMat[:, -1][scoreMat[:, -1] < 1] = 1
 
     # Normalize Scores:
     scaler = MinMaxScaler()
@@ -223,8 +221,9 @@ def main():
 
     testData = _parseTestingData()
     results = _runScoring(clusters, testData)
-    outfile = open("data/phad_ablation.csv", "wb")
-    writer = csv.writer(outfile)
+    #  outfile = open("data/phad_ablation.csv", "wb")
+    #  writer = csv.writer(outfile)
+    import pdb; pdb.set_trace()
     _outputToCSV(results, "data/phad_results.csv", threshold=0.5, feat=None)
     data = check_results('data/phad_results.csv',
                          'data/master-listfile-condensed.txt',
@@ -232,20 +231,20 @@ def main():
                          False,
                          False)
     print(">>> %s %f" % ("All", max(data['f1s'])))
-    writer.writerow(["All", max(data['f1s'])])
-
-    for feat in xrange(33):
-        results = _runScoring(clusters, testData)
-        _outputToCSV(results, "data/phad_results.csv", threshold=0.5, feat=feat)
-        data = check_results('data/phad_results.csv',
-                             'data/master-listfile-condensed.txt',
-                             '0.60:0.80:400',
-                             False,
-                             False)
-        print(">>> %s %f" % (FEATURES[feat], max(data['f1s'])))
-        writer.writerow([FEATURES[feat], max(data['f1s'])])
-
-    outfile.close()
+    #  writer.writerow(["All", max(data['f1s'])])
+    #
+    #  for feat in xrange(33):
+    #      results = _runScoring(clusters, testData)
+    #      _outputToCSV(results, "data/phad_results.csv", threshold=0.5, feat=feat)
+    #      data = check_results('data/phad_results.csv',
+    #                           'data/master-listfile-condensed.txt',
+    #                           '0.60:0.80:400',
+    #                           False,
+    #                           False)
+    #      print(">>> %s %f" % (FEATURES[feat], max(data['f1s'])))
+    #      writer.writerow([FEATURES[feat], max(data['f1s'])])
+    #
+    #  outfile.close()
 
 
 if __name__ == '__main__':
