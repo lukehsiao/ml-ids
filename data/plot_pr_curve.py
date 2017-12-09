95a7c467e41b589126c200149c8826a16b29df32
Luke Hsiao <lwhsiao@stanford.edu>
2017-12-09 00:50:35 -0800

    Adjust aspect and scale for poster

--- data/plot_pr_curve.py
+++ data/plot_pr_curve.py
@@ -2,10 +2,12 @@ import pandas as pd
 import seaborn as sns
 from matplotlib.backends.backend_pdf import PdfPages
 
+
+sns.set(font_scale=3.0, style="ticks")
 data = pd.read_csv("pr_curve.csv")
 
 plot = sns.lmplot(x="recall", y="precision", data=data, fit_reg=False,
-                  markers=".")
+                  markers=".", scatter_kws={"s": 200}, aspect=2)
 plot.set(ylim=(0, 1))
 plot.set(xlim=(0, 1))
 plot.set(ylabel="Precision")
@@ -18,7 +20,7 @@ pp.close()
 data2 = pd.read_csv("../temp.csv")
 
 plot2 = sns.lmplot(x="threshold", y="f1", data=data2, fit_reg=False,
-                   markers=".")
+                   markers=".", scatter_kws={"s": 200}, aspect=2)
 
 plot2.set(ylim=(0, 1))
 plot2.set(xlim=(0.5, 1))
