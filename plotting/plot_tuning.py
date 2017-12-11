b80c5c7111803cfaf54b6c196fa73b2bda81740b
Luke Hsiao <lwhsiao@stanford.edu>
2017-12-10 23:02:43 -0800

    Update the tuning code to calculate cols itself
    Instead of needing to populate the CSV columns manually.

--- plotting/plot_tuning.py
+++ plotting/plot_tuning.py
@@ -8,9 +8,16 @@ import seaborn as sns
 
 sns.set(font_scale=3.0, style="ticks")
 data = pd.read_csv("../data/phad_tuning.csv")
+DOT_SIZE = 50
 
-plot = sns.lmplot(x="recall", y="precision", data=data, fit_reg=False,
-                  markers=".", scatter_kws={"s": 200}, aspect=2)
+data["tp"] = data.apply(lambda row: row.attacksdetected / 100 * 201, axis=1)
+data["recall"] = data.apply(lambda row: row.attacksdetected / 100, axis=1)
+data["prec"] = data.apply(lambda row: row.tp / (row.tp + row.TotalNumFP), axis=1)
+data["f1"] = data.apply(lambda row: (2 * (row.recall * row.prec) /
+                                     (row.recall + row.prec)), axis=1)
+
+plot = sns.lmplot(x="recall", y="prec", data=data, fit_reg=False,
+                  markers=".", scatter_kws={"s": DOT_SIZE}, aspect=2)
 plot.set(ylim=(0, 1))
 plot.set(xlim=(0, 1))
 plot.set(ylabel="Precision")
@@ -21,7 +28,7 @@ pp.savefig(plot.fig)
 pp.close()
 
 plot = sns.lmplot(x="threshold", y="f1", data=data, fit_reg=False,
-                   markers=".", scatter_kws={"s": 200}, aspect=2)
+                   markers=".", scatter_kws={"s": DOT_SIZE}, aspect=2)
 
 plot.set(ylim=(0, 1))
 plot.set(xlim=(0.5, 1))
@@ -32,11 +39,11 @@ pp.savefig(plot.fig)
 pp.close()
 
 plot = sns.lmplot(x="TotalNumFP", y="recall", data=data,
-                  fit_reg=False, markers=".", scatter_kws={"s": 200},
+                  fit_reg=False, markers=".", scatter_kws={"s": DOT_SIZE},
                   aspect=2)
 
 plot.set(ylim=(0, 1))
-plot.set(xlim=(0, 200))
+plot.set(xlim=(0, 150))
 plot.set(ylabel=r"Recall")
 plot.set(xlabel="Num False Positives")
 pp = PdfPages("dfa_curve.pdf")
