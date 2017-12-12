75c345893e9a51ea398aa8dc1f9260550f6c82a5
Luke Hsiao <lwhsiao@stanford.edu>
2017-12-12 15:11:01 -0800

    Rotate ablation study plots

--- plotting/plot_phad_ablation.py
+++ plotting/plot_phad_ablation.py
@@ -7,16 +7,18 @@ import pandas as pd
 import seaborn as sns
 
 sns.set(style="whitegrid")
-fig, ax = plt.subplots(figsize=(4, 6))
+fig, ax = plt.subplots(figsize=(6, 4))
 
 data = pd.read_csv("../data/phad_ablation.csv")
 data = data.sort_values("f1", ascending=True)
 
 sns.set_color_codes("muted")
-sns.barplot(y="ablated", x="f1", data=data, palette=data["color"], ax=ax)
-ax.set_ylabel("Ablated Feature")
-ax.set_xlabel("F1 Score")
-ax.set_xlim((0.23, 0.40))
+sns.barplot(y="f1", x="ablated", data=data, palette=data["color"], ax=ax)
+ax.set_xlabel("Ablated Feature")
+ax.set_ylabel("F1 Score")
+ax.set_ylim((0.00, 0.40))
+for item in ax.get_xticklabels():
+    item.set_rotation(90)
 sns.despine(left=True, bottom=True)
 plt.setp(fig.axes)
 plt.tight_layout(h_pad=3)
@@ -25,14 +27,16 @@ pp.savefig(fig)
 pp.close()
 plt.close(fig)
 
-fig, ax = plt.subplots(figsize=(4, 6))
+fig, ax = plt.subplots(figsize=(6, 4))
 data = pd.read_csv("../data/phad_compounding_ablation.csv")
 
 sns.set_color_codes("muted")
-sns.barplot(y="ablated", x="f1", data=data, palette=data["color"], ax=ax)
-ax.set_ylabel("Additionally Removed Feature (from All to 1)")
-ax.set_xlabel("F1 Score")
-ax.set_xlim((0.00, 0.40))
+sns.barplot(y="f1", x="ablated", data=data, palette=data["color"], ax=ax)
+ax.set_xlabel("Additionally Removed Feature (from All to 1)")
+ax.set_ylabel("F1 Score")
+ax.set_ylim((0.00, 0.40))
+for item in ax.get_xticklabels():
+    item.set_rotation(90)
 sns.despine(left=True, bottom=True)
 plt.setp(fig.axes)
 plt.tight_layout(h_pad=3)
